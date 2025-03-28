532487d5f0e7d43a8bf043deeb53794fe1ee0c2a
AKing090 <ammarabdulkadir396@gmail.com>
2025-03-28 10:11:47 +0100

    javacsrpt
    javasrcipt link in html
    
    javascript bestand gemaakt en toegevoegd

--- js/javascript.js
+++ js/javascript.js
@@ -29,22 +29,4 @@ document
 
 
   
-  document.getElementById("vacatureForm").addEventListener("submit", function(event) {
-      event.preventDefault(); 
-
-      
-      const name = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
-  
-
-      
-      const table = document.querySelector("table");
-      const newRow = table.insertRow();
-      
-    
-      newRow.insertCell(0).textContent = name;
-      newRow.insertCell(1).textContent = email;
-   
-  
-      document.getElementById("vacatureForm").reset();
-  });
+ 
